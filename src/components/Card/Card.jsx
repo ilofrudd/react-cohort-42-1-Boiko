9bf89a7698a033f66bbe27c2603274b515869c69
ilofrudd <boyko5000@gmail.com>
2024-09-20 18:47:31 +0200

    Lesson 03

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,13 @@
 import "./styles.css";
 
 function Card() {
-    const user = {
+  const user = {
     firstName: "Homer",
     lastName: "Simpson",
-    age: 39,
     job: "Safety Inspector",
     hobby: "Bowling",
-    isAdmin: false,
     avatarURL:
-    "https://i.pinimg.com/originals/0f/e9/77/0fe9776fbeccdeddf3ee30dac0a0cdc5.jpg",
+      "https://i.pinimg.com/originals/0f/e9/77/0fe9776fbeccdeddf3ee30dac0a0cdc5.jpg",
   };
 
   const getFullName = () => {
@@ -18,15 +16,11 @@ function Card() {
 
   return (
     <div className="card-container">
-        <p className="card-info">User Card</p>
-        <img className="avatar" src={user.avatarURL} alt="User Avatar" />
-        <p className="card-info">Full Name: {getFullName()}</p>
-        <p className="card-info">Age: {user.age}</p>
-        <p className="card-info">Job: {user.job}</p>
-        <p className="card-info">Hobby: {user.hobby}</p>
-        <p className="card-info">
-          Role: {user.isAdmin ? "Admin" : "Not Admin"}
-        </p>
+      <p className="card-info">User Card</p>
+      <img className="avatar" src={user.avatarURL} alt="User Avatar" />
+      <p className="card-info">Full Name: {getFullName()}</p>
+      <p className="card-info">Job: {user.job}</p>
+      <p className="card-info">Hobby: {user.hobby}</p>
     </div>
   );
 }
