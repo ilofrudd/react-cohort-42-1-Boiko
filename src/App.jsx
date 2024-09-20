288563dd689e79c13b1c73d71913344948f98d7c
ilofrudd <boyko5000@gmail.com>
2024-09-20 18:52:07 +0200

    Lesson04

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@
 // 3 шаг - импортироуем компонент Lesson_02
 import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
+import Lesson_04 from "./lessons/Lesson_04/Lesson_04";
 
 // HOMEWORKS
 import Homework_02 from "./homeworks/Homework_02/Homework_02";
+import Homework_03 from "./homeworks/Homework_03/Homework_03";
 
 // CONSULTATIONS
 
@@ -21,9 +23,11 @@ function App() {
       {/* Lessons */}
       {/* 4 шаг - возвращаем компонент Lesson_02 из компонента App */}
       {/* <Lesson_02 /> */}
-      <Lesson_03 />
+      {/* <Lesson_03 /> */}
+      <Lesson_04 />
       {/* Homeworks */}
       {/* <Homework_02 /> */}
+      {/* <Homework_03 /> */}
     </div>
   );
 }
